e530f120c6553d0aff81a82daa6074ba17930b0b
Allen Wyma <allen.wyma@gmail.com>
2016-02-22 14:23:23 +0800

    fixed error in route and fixed up loading spinner

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
   post '/' => 'welcome#file_upload'
-  get '/:upload_id' => 'welcome#show'
+  get '/:upload_id' => 'welcome#show', as: :show
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
